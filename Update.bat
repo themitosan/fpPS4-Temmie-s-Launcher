0c92ad070a2c7d279bd328ca1b1fb47388ebc2bc
themitosan <juliodogers@gmail.com>
2024-04-05 19:39:13 -0300

    Updater: small hotfix

--- Update.bat
+++ Update.bat
@@ -1,9 +1,10 @@
 @echo off
+cls
 title Updating fpPS4 Temmie's Launcher - Please Wait...
-echo .
+echo.
 echo Updating fpPS4 Temmie's Launcher - Please wait
 echo IMPORTANT: Make sure to have Git installed on your OS!
-echo .
+echo.
 git reset --hard
 git pull
 pause
